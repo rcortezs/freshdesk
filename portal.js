c14d00d2b49c4af5d3d3c2b36e3fe6ae2e1fe17b
Roberto Cortez <rcortez@productos-iot.mx>
2021-05-11 12:03:39 -0500

    Un solo portal

---
+++ portal.js
@@ -0,0 +1,6 @@
+console.log("{{portal.name}}");
+console.log("{{portal.logo_url}}");
+console.log("{{portal.login_url}}");
+console.log("{{portal.signup_url}}");
+console.log("{{portal.new_ticket_url}}");
+console.log("{{portal.current_page}}")
