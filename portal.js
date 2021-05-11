c0db45d96cfe01d557fc03e033accdf0855b8c7b
rcortezs <84032466+rcortezs@users.noreply.github.com>
2021-05-11 12:20:57 -0500

    Update portal.js

--- portal.js
+++ portal.js
@@ -4,3 +4,10 @@ console.log({{portal.login_url}});
 console.log({{portal.signup_url}});
 console.log({{portal.new_ticket_url}});
 console.log({{portal.current_page}})
+
+{{portal.name}}
+{{portal.logo_url}}
+{{portal.login_url}});
+{{portal.signup_url}}
+{{portal.new_ticket_url}}
+{{portal.current_page}}
